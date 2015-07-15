a6ada8e510eeef9c3e832f4b6cb09bdd1db29a65
peoplma <peoplma@gmail.com>
2015-07-15 13:26:55 -0400

    Update Randomdoge.py

--- Randomdoge.py
+++ Randomdoge.py
@@ -37,11 +37,14 @@ def pick_random_comment():
     for comment in subreddit_comments:
         op_text = comment.body
         has_praw = any(string in op_text for string in prawWords)
-        if comment.id not in already_done and has_praw:
+        author = comment.author
+		if not comment.author:
+			continue
+		else:	
+			if comment.id not in already_done and has_praw and author.name != 'Randomactofdogebot':
             comment.reply('This is a random act of doge! +/u/dogetipbot ' + str(rand_amount(amount_min, amount_max)) + ' doge\n\nPlease consider tipping this bot to keep it running!\n\n[Bot Info](http://www.reddit.com/r/dogecoin/comments/1yi0s1/all_the_information_you_need_to_know_about_me/) ---- [Source Code](https://github.com/Healdb/random_act_of_doge_bot)')
             already_done.add(comment.id)
             break
-
 def lucky_tip(message, amount):
     if amount < 100.1:  ## Stop mega payouts
         message.reply("Your tip was a lucky tip!\n\n+/u/dogetipbot " + str(amount * 2) + " doge")
